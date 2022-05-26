1150a08056aa603f0144c7aeee20aa84d7fedb64
Mohotasir <talukdertahsin643@gmail.com>
2022-05-26 13:23:53 +0600

    add new code

--- js/script.js
+++ js/script.js
@@ -11,3 +11,21 @@ window.onscroll = function (){
     }
 }
 
+// connect form
+let imgId =document.querySelector('#connect');
+let form = document.querySelector('form');
+let icon = document.querySelector('.icon');
+let btn = document.querySelector('#sbmt-btn');
+let displayForm = function(){
+    form.style.display ='block';
+    form.style.transition = .5;
+}
+let hideForm = function(){
+    form.style.display = 'none';  
+    
+}
+
+
+
+imgId.addEventListener("click",displayForm);
+icon.addEventListener('click',hideForm);
