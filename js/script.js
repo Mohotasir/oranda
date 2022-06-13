d3ea23d42609621d2fa7eb7ea8e332b3cb439e2f
mohotasir <talukdertahsin643@gmail.com>
2022-06-12 18:38:56 -0700

    commited

--- js/script.js
+++ js/script.js
@@ -3,14 +3,40 @@
 // sticky navbar 
 let mainNav = document.querySelector('#main-nav');  
 window.onscroll = function (){
-    if(window.scrollY>=200){
+    if(window.scrollY>=100){
         mainNav.classList.add('sticky');
+       
         
     }
     else{
         mainNav.classList.remove('sticky');
     }
 }
+
+//preloader
+
+let loader = document.querySelector('#loader');
+let show = function(){
+    loader.style.display = "none";
+
+}
+window.addEventListener("load",show);
+//icon clcik
+
+let icons = document.querySelector('.icon-nav i');
+let menu = document.querySelector('.menu');
+let cutIcon = document.querySelector('.cut-nav i');
+
+let showMenu = function(){
+    menu.style.display =' block';
+    icons.style.display='none';
+}
+
+icons.addEventListener("click",showMenu);
+cutIcon.addEventListener("click",function(){
+    menu.style.display = 'none';
+    icons.style.display = 'block';
+});
 
 // connect form
 let imgId =document.querySelector('#connect');
